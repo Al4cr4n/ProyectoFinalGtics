e2100a350011ab3e7ab71e62f80c1c90dd00297e
Jostin <a20213830@pucp.edu.pe>
2024-09-24 17:52:51 -0500

    controller simple para verificar vistas y cambio de href de inicio_superadmin

---
+++ TelExpress/src/main/java/com/example/telexpress/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.telexpress.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("")
+    public String index(){
+
+        return "SuperAdmin/inicio_superadmin";
+    }
+}
