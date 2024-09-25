723b4e7b38fbe0d6d6e4f71d26630eed64160e56
ViniTvll <vinitavella18@gmail.com>
2024-09-25 18:40:20 -0500

    Imports en SuperAdminController

--- TelExpress/src/main/java/com/example/telexpress/controller/SuperAdminController.java
+++ TelExpress/src/main/java/com/example/telexpress/controller/SuperAdminController.java
@@ -1,9 +1,13 @@
 package com.example.telexpress.controller;
 
+import com.example.telexpress.entity.Producto;
 import com.example.telexpress.entity.Usuario;
 import com.example.telexpress.entity.Zona;
 import com.example.telexpress.repository.AdminRepository;
 import com.example.telexpress.repository.ZonaRepository;
+import com.example.telexpress.repository.UsuarioRepository;
+import com.example.telexpress.repository.ProductoRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.List;
 @Controller
