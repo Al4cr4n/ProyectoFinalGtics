cbef9efefe15e0f7bc0216978a7d7ff3de41cdbf
IsaacAHS <isaac.huamani@pucp.edu.pe>
2024-09-25 00:59:24 -0500

    superadmin repositorio

---
+++ TelExpress/src/main/java/com/example/telexpress/repository/AdminRepository.java
@@ -0,0 +1,28 @@
+package com.example.telexpress.repository;
+
+import com.example.telexpress.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Usuario,Integer>{
+    /*Query para obtener a todos los usuarios por su id_rol=4(usuario final)*/
+    @Query(value = "select * from usuario where roles_idRoles= 4",
+            nativeQuery= true)
+    List<Usuario> buscarUsuarioPorRol();
+
+    /*Query para obtener a todos los agentes por su id_rol=3(agente)*/
+    @Query(value = "select * from usuario where roles_idRoles= 3",
+            nativeQuery= true)
+    List<Usuario> buscarAgentePorRol();
+
+    /*Query para obtener a todos los coordis por su id_rol=2()*/
+    @Query(value = "select * from usuario where roles_idRoles= 2",
+            nativeQuery= true)
+    List<Usuario> buscarCoordiPorRol();
+
+
+}
