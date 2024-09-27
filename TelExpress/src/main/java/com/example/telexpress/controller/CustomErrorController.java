bd77fc8c5323440633c9b7ad8145e0a2c6148f4c
NikolMontes <a20203644@pucp.edu.pe>
2024-09-27 06:04:43 -0500

    crear zonal casi

--- TelExpress/src/main/java/com/example/telexpress/controller/CustomErrorController.java
+++ TelExpress/src/main/java/com/example/telexpress/controller/CustomErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
 @Controller
 public class CustomErrorController implements ErrorController {
 
@@ -41,3 +41,4 @@ public class CustomErrorController implements ErrorController {
     }
 
 }
+*/
