e9c39eae917ac9c152baad10cb1aa9b5ede3da5c
lia2003 <a20210779@pucp.edu.pe>
2024-09-30 21:36:17 -0500

    coordinador controller

---
+++ TelExpress/src/main/java/com/example/telexpress/controller/CoordinadorController.java
@@ -0,0 +1,35 @@
+package com.example.telexpress.controller;
+import com.example.telexpress.entity.*;
+import com.example.telexpress.repository.*;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.List;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+@RequestMapping("/coordinador")
+public class CoordinadorController {
+    final AdminRepository adminRepository;
+    final ZonaRepository zonaRepository;
+    final ProductoRepository productoRepository;
+    final UsuarioRepository usuarioRepository;
+    final ProveedorRepository proveedorRepository;
+    final OrdenesRepository ordenesRepository;
+
+
+    public CoordinadorController(AdminRepository adminRepository, ZonaRepository zonaRepository,
+                                ProductoRepository productoRepository, UsuarioRepository usuarioRepository,
+                                ProveedorRepository proveedorRepository, OrdenesRepository ordenesRepository,
+                                 AgenteRepository
+    ) {
+        this.adminRepository=adminRepository; this.zonaRepository=zonaRepository;
+        this.productoRepository=productoRepository; this.usuarioRepository=usuarioRepository;
+        this.proveedorRepository=proveedorRepository; this.ordenesRepository=ordenesRepository;
+
+    }
+}
