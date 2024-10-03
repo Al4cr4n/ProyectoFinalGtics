eefeefe4892a18cfd071695e9f0419d8512904c1
NikolMontes <a20203644@pucp.edu.pe>
2024-10-03 14:23:42 -0500

    proceso de login, luego se adapta

--- TelExpress/src/main/java/com/example/telexpress/config/SecurityWeb.java
+++ TelExpress/src/main/java/com/example/telexpress/config/SecurityWeb.java
@@ -7,11 +7,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.authorization.AuthenticatedAuthorizationManager.authenticated;
 
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityWeb {
+
+    final DataSource dataSource;
+    public SecurityWeb(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -24,11 +35,30 @@ public class SecurityWeb {
                 );
 
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers ("/agente","/agente/**").authenticated()
-                .requestMatchers("/superadmin","/superadmin/**").authenticated()
+                .requestMatchers ("/agente","/agente/**").hasAuthority("Agente") //acceso solo para agentes
+                .requestMatchers("/superadmin","/superadmin/**").hasAuthority("Superadmin")  //acceso solo para superadmin
                 .anyRequest().permitAll()
         );
         return http.build();
+    }
+
+    /*@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }*/
+
+    public UserDetailsManager userDetailsManager(){
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+        String sql1 = "select correo, contrasena, (CASE WHEN isBan = 0 THEN 1  ESLSE 0 END) as activo FROM usuario WHERE correo = ?" ;
+        String sql2 = "select u.correo, r.roles FROM usuario u INNER JOIN roles r ON (u.idroles = r.idRoles)" +
+                "WHERE u.correo = ? AND us.isBan = 0";
+
+        users.setUsersByUsernameQuery(sql1);
+        users.setAuthoritiesByUsernameQuery(sql2);
+
+        return users;
+
     }
 
 }
