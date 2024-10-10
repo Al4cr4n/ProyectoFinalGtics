ff32a279ec439aa9ab026a46a2213ebbedf5d923
lia2003 <a20210779@pucp.edu.pe>
2024-10-10 12:28:16 -0500

    entity usuario corregida

--- TelExpress/src/main/java/com/example/telexpress/entity/Usuario.java
+++ TelExpress/src/main/java/com/example/telexpress/entity/Usuario.java
@@ -1,25 +1,27 @@
 package com.example.telexpress.entity;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import  java.util.List;
+import java.util.List;
 import java.util.ArrayList;
 
 @Getter
 @Setter
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="idusuario")
+    @Column(name = "idusuario")
     private Integer id;
 
     @Column(nullable = false)
     private String nombre;
+
     private String apellido;
     private String correo;
     private String direccion;
@@ -28,57 +30,59 @@ public class Usuario {
     private String contrasena;
     private String estadoUsuario;
     private String motivo;
-/*
-    @Column(name = "cantidadcompras")
-    private int cantidadcompras;
+    private String ruc;
 
- */
+    @Column(name = "notificaciones", length = 1024)
+    private String notificaciones;
 
-    @Column(name="razonSocial")
+    @Column(name = "razonSocial", length = 100)
     private String razonSocial;
-
 
     @Column(name = "fechanacim", nullable = true)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechanacimiento;
 
-    private String ruc;
-
     @ManyToOne
-    @JoinColumn(name= "idroles")
+    @JoinColumn(name = "idroles")
     private Rol rol;
 
     @ManyToOne
-    @JoinColumn(name="distritos_iddistritos", nullable = true)
+    @JoinColumn(name = "distritos_iddistritos", nullable = true)
     private Distrito distrito;
 
     @ManyToOne
-    @JoinColumn(name="idzona", nullable = false)
+    @JoinColumn(name = "idzona", nullable = false)
     private Zona zona;
 
     @ManyToOne
-    @JoinColumn(name= "idproveedor", nullable = true)
+    @JoinColumn(name = "idproveedor", nullable = true)
     private Proveedor proveedor;
 
     @ManyToOne
-    @JoinColumn(name="despachador_iddespachador", nullable = true)
+    @JoinColumn(name = "despachador", nullable = true)
     private CodigoDespachador codigoDespachador;
 
     @ManyToOne
-    @JoinColumn(name="idSuperior", nullable = true)
+    @JoinColumn(name = "idSuperior", nullable = true)
     private Usuario idSuperior;
 
     @Column(nullable = false)
     private Integer isBan = 0; // Inicializa con 0 por defecto
 
-    /*@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<Ordenes> ordenes;*/
+    /* Comentado según la solicitud */
+    //@Column(name = "cantidadcompras")
+    //private int cantidadcompras;
+
+    //FOTOS
+    /*
+    @ManyToOne
+    @JoinColumn(name = "fotos_idfotos", nullable = true)
+    private Foto foto;*/
 
     @ManyToOne
-    @JoinColumn(name="jurisdiccion_idjurisdiccion")
+    @JoinColumn(name = "jurisdiccion", nullable = true)
     private Jurisdiccion jurisdiccion;
 
     @OneToMany(mappedBy = "usuario")
     private List<Ordenes> ordenes = new ArrayList<>();
-
 }
