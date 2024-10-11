8e1a2b73ae8e38f6498a468d54cd5e707ea5f904
ViniTvll <vinitavella18@gmail.com>
2024-10-11 13:52:24 -0500

    Arreglada tabla en lista_pedidos al 70%

--- TelExpress/src/main/java/com/example/telexpress/controller/UsuarioController.java
+++ TelExpress/src/main/java/com/example/telexpress/controller/UsuarioController.java
@@ -283,6 +283,8 @@ public class UsuarioController {
     public String lista_pedidos(@RequestParam(value = "search", required = false) String search, Model model) {
         List<Ordenes> ordenes = ordenesRepository.findOrdenesByUsuario(18);
         List<Usuario> usuarios = usuarioRepository.findAll();
+        List<String> estadosTodos = Arrays.asList("EN PROCESO", "ARRIBO AL PAÍS", "EN ADUANAS", "EN RUTA", "RECIBIDO", "EN VALIDACIÓN", "CREADO","EN PROCESO");
+        ordenes = ordenesRepository.findByEstadoOrdenesIn(estadosTodos);
 
         // Obtener el agente asignado a cada usuario a través del idsuperior
         Map<Integer, String> agentesMap = new HashMap<>();
