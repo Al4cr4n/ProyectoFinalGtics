c0dc0e2d981eb4d588d0d70cfbf44625767ab41b
Al4cr4n <148933724+Al4cr4n@users.noreply.github.com>
2024-10-14 00:33:12 -0500

    cambio Diego

--- TelExpress/src/main/java/com/example/telexpress/entity/Resenia.java
+++ TelExpress/src/main/java/com/example/telexpress/entity/Resenia.java
@@ -1,36 +1,50 @@
 package com.example.telexpress.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import java.sql.Blob;
 
 @Entity
 @Getter
 @Setter
-@Table(name ="resenias")
+@Table(name = "resenias")
 public class Resenia {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idresenias;
+
+    @Column(length = 500)
     private String descripcion;
+
+    @Column(length = 500)
     private String calidad;
+
+    @Column(length = 500)
+    private String rapidez;
+
     private int puntuacion;
+
+    @Lob
+    private Blob foto;
+
     @ManyToOne
     @JoinColumn(name = "productos_idproductos", nullable = false)
     private Producto producto;
+
     @ManyToOne
-    @JoinColumn(name = "usuario_idusuario", nullable = true)
+    @JoinColumn(name = "usuario_idusuario", nullable = true, foreignKey = @ForeignKey(name = "fk_resenias_usuario1"))
     private Usuario usuario;
 
-    @Column(name="tituloForo", nullable=true)
-    private String titulo;
+    @Column(name = "tituloresena", length = 200)
+    private String tituloresena;
 
-    @Column(name="tipoPublicacion", nullable=true)
-    private String tipo;
-
-
-
-
-
+    @Column(name = "tipoPublicacion", length = 45, nullable = false)
+    private String tipoPublicacion;
 }
+
+
+
+
+
